d9050c29d99c527c29b66766a4040fbe54a8bdcc
JohnWeeks1 <jonathanweeks007@gmail.com>
2023-02-12 22:26:13 +0700

    update photo flex

--- resources/js/Pages/User.vue
+++ resources/js/Pages/User.vue
@@ -171,9 +171,13 @@ defineProps({ posts: Object, user: Object })
                     <div class="bg-white p-3 mt-4 rounded-lg shadow-lg">
                         <div class="font-extrabold pb-2 text-xl">Photos</div>
                         <div class="flex flex-wrap items-center justify-start w-full">
-                            <span v-for="photo in posts.data" :key="photo" class="w-1/3">
+                            <span
+                                v-for="photo in posts.data"
+                                :key="photo"
+                                class="w-1/3"
+                                v-show="photo.image !== null"
+                            >
                                 <img
-                                    v-if="photo.image"
                                     @click="isImageDisplay = photo.image"
                                     class="aspect-square object-cover p-1 rounded-lg cursor-pointer"
                                     :src="photo.image"
